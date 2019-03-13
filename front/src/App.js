3318be4616ab42dab833117dddd3d512c95a9a9e
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2019-03-13 20:10:16 +0100

    feat: adds MovieOrSpinner for branch demo

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Movie } from 'components'
-
+import { Movie, Spinner } from 'components'
+import { branch } from 'hoc'
+import { propEq } from "ramda"
 import './App.css';
 
 export default () => {
 
+  const MovieOrSpinner = branch(propEq('title', "Annihilation") ,Spinner, Movie)
+
   return (
     <div className="App">
-      <Movie title="Annihilation" year="2018" poster="https://m.media-amazon.com/images/M/MV5BMTk2Mjc2NzYxNl5BMl5BanBnXkFtZTgwMTA2OTA1NDM@._V1_SX300.jpg" totalVotes={343} />
-      <Movie title="I Am Not a Witch" year="2019" poster="https://m.media-amazon.com/images/M/MV5BMmZiMmEzMzAtYzhiZi00NjMwLWJmNzMtMjRkNTI2YTM5N2FiXkEyXkFqcGdeQXVyNTkyMjQwNw@@._V1_SX300.jpg" totalVotes={0} />
+      <MovieOrSpinner title="Annihilation" year="2018" poster="https://m.media-amazon.com/images/M/MV5BMTk2Mjc2NzYxNl5BMl5BanBnXkFtZTgwMTA2OTA1NDM@._V1_SX300.jpg" totalVotes={343} />
+      <MovieOrSpinner title="I Am Not a Witch" year="2019" poster="https://m.media-amazon.com/images/M/MV5BMmZiMmEzMzAtYzhiZi00NjMwLWJmNzMtMjRkNTI2YTM5N2FiXkEyXkFqcGdeQXVyNTkyMjQwNw@@._V1_SX300.jpg" totalVotes={0} />
     </div>
   );
 };
